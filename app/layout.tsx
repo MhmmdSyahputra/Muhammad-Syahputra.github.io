66319d8cabc6e1e39d730db780aeb4535aea3c4f
MhmmdSyahputra <putra.cokers@gmail.com>
2023-12-26 01:30:17 +0700

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Muhammad Syahputra</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
